3fca313fd5a331ef49cfc42c6b71f219bb2e02ed
Grzegorz Janosz <gregory.janosz@gmail.com>
2024-07-04 00:15:51 +0200

    Add initial xcode project

---
+++ Rick and Morty Revealed/Rick_and_Morty_RevealedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_Morty_RevealedApp.swift
+//  Rick and Morty Revealed
+//
+//  Created by Grzegorz Janosz on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_Morty_RevealedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
